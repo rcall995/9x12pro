805af1305a2196e35215c7bfef92e815cf041466
Rich <rcall14072@gmail.com>
2026-01-09 19:50:38 -0500

    Add Outreach Scripts page with 4-email sequence and social templates (v237)

--- config.js
+++ config.js
@@ -36,7 +36,7 @@
 
     // App Configuration
     app: {
-      version: "2026-01-09-v236",
+      version: "2026-01-09-v237",
       environment: window.ENV_ENVIRONMENT || "production",
       enableDebugLogs: window.ENV_DEBUG === 'true' || false
     },
