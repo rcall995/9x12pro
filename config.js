54f2c0d534d989f441083b87849e9c2bb41d9f24
Rich <rcall14072@gmail.com>
2026-01-10 14:39:08 -0500

    Add customer update email templates (v262)

--- config.js
+++ config.js
@@ -36,7 +36,7 @@
 
     // App Configuration
     app: {
-      version: "2026-01-10-v261",
+      version: "2026-01-10-v262",
       environment: window.ENV_ENVIRONMENT || "production",
       enableDebugLogs: window.ENV_DEBUG === 'true' || false
     },
