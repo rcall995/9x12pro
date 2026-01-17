ae38c5d552e81e067a15292a687018e82793fe1a
Rich <rcall14072@gmail.com>
2026-01-17 15:19:47 -0500

    Fix Facebook/Instagram - open Messenger (m.me) instead of page (v299)

--- config.js
+++ config.js
@@ -36,7 +36,7 @@
 
     // App Configuration
     app: {
-      version: "2026-01-17-v298",
+      version: "2026-01-17-v299",
       environment: window.ENV_ENVIRONMENT || "production",
       enableDebugLogs: window.ENV_DEBUG === 'true' || false
     },
