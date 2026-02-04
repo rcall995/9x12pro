509e788d45cad5d8a0cf46ca716ec1f753843eca
Rich <rcall14072@gmail.com>
2026-02-04 15:31:02 -0500

    Bump version to v490

--- config.js
+++ config.js
@@ -36,7 +36,7 @@
 
     // App Configuration
     app: {
-      version: "2026-02-04-v483",
+      version: "2026-02-04-v490",
       environment: window.ENV_ENVIRONMENT || "production",
       enableDebugLogs: window.ENV_DEBUG === 'true' || false
     },
