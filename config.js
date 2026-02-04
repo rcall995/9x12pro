05a6f21b9732924b0d0f6c8375891adc59778fa4
Rich <rcall14072@gmail.com>
2026-02-03 23:57:18 -0500

    Make CRM card close button more visible (v478)

--- config.js
+++ config.js
@@ -36,7 +36,7 @@
 
     // App Configuration
     app: {
-      version: "2026-02-03-v477",
+      version: "2026-02-03-v478",
       environment: window.ENV_ENVIRONMENT || "production",
       enableDebugLogs: window.ENV_DEBUG === 'true' || false
     },
