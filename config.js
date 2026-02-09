c7ad597b39d1d76f87fcf729059c26e108689190
Rich <rcall14072@gmail.com>
2026-02-09 16:07:20 -0500

    Bump version to v576

--- config.js
+++ config.js
@@ -37,7 +37,7 @@
 
     // App Configuration
     app: {
-      version: "2026-02-09-v575",
+      version: "2026-02-09-v576",
       environment: window.ENV_ENVIRONMENT || "production",
       enableDebugLogs: window.ENV_DEBUG === 'true' || false
     },
